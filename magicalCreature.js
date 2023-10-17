60eee79d4e747f74359c57a431173bffc278db42
pinabrandon <114242621+pinabrandon@users.noreply.github.com>
2023-10-17 23:27:45 +0000

    learned about variablese and about operators.

--- magicalCreature.js
+++ magicalCreature.js
@@ -1,40 +1,40 @@
 // Welcome to the magical zoo! Your task is to create and manage a list of magical creatures. Let's have some fun!
 
 // 1. Declare a variable named 'creatureName' and assign it a magical creature's name as a string.
-
+let creaturename = "dragon";
 
 
 // 2. Declare a variable named 'creatureDescription' and describe its magical properties as a string.
-
+let creatureDescription= "a reptile that breathes fire";
 
 // 3. Do you believe that this creature is friendly to humans? 
 // Declare a variable named 'isFriendly' and assign it a boolean value.
-
+let isfriendly = "friendly";
 
 
 // 4. The magical zoo has an age restriction! Declare a variable named 'visitorAge' and assign it a random age between 1 and 60.
 // Hint: Use Math.floor() to round a number up.
 // Hint: Use Math.random() for generating random numbers.
-
+let visitorAge = Math.floor(Math.random() * 60) + 1;
 
 
 // 5. Determine if the visitor is allowed to see the creature.
 // Declare a variable named 'canVisit' and assign it an expression to see 
 // if the visitorAge variable is greater than 15.
 // Hint: use this resource if you need support: https://www.w3schools.com/js/js_booleans.asp 
-
+let canVisit = visitorAge > 15;
 
 
 // 6. Combine all the information into a summary.
 // Declare a variable named 'zooSummary' and combine all the above information using concatenation.
-
+let zooSummary = "The " + creaturename + "is  " + creatureDescription + ". it is "+ isfriendly + " and it is " + visitorAge + " years old.";
 
 // 7. Print the summary using console.log()
-
-
+console.log(zooSummary);
 
 // Challenge:
 // 8. Now, imagine there are three more creatures in the zoo. 
 //    Repeat Tasks 1-6 for each creature, then combine all summaries into a single variable 'allZooSummaries'.
+
 // 9. Print the 'allZooSummaries' to see the details of all the creatures in the magical zoo.
 
--- operators.js
+++ operators.js
@@ -3,26 +3,35 @@
 
 // 1. Addition
 // Create two variables, num1 and num2, and assign them any two numbers of your choice.
-// Add the two numbers and store the result in a new variable called sum.
+let num1 = 7;
+let num2 = 9;
+// Add the two numbers and store the result in a new variable called sum. 
+let sum = num1 + num2
 // Print the result using console.log().
+ console.log(sum)
 
 
 
 // 2. Subtraction
 // Subtract num2 from num1 and store the result in a new variable called difference.
+let sum2 = num2 - num1
 // Print the result.
-
+console.log(sum2)
 
 // 3. Multiplication
 // Multiply num1 by num2 and store the result in a new variable called product.
+let sum3 = num1 * num2
 // Print the result.
-
+console.log(sum3)
 
 
 // String Operations
 
 // 4. Concatenation with +
 // Create two string variables, str1 and str2, and assign them any two strings of your choice.
+let s1 = "Hello"
+let s2 = "World"
 // Concatenate (combine) the two strings using the + operator and store the result in a new variable called combinedString.
+let s3= s1 + s2
 // Print the result.
-
+console.log(s3)
